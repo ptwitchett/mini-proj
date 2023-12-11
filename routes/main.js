62d73179e4cd58e6493a92efe9e9f5f4e706504a
ptwitchett <p.twitchett@aol.co.uk>
2023-12-11 16:21:52 +0000

    initial version

---
+++ routes/main.js
@@ -0,0 +1,123 @@
+module.exports = function(app, shopData) {
+
+    // Handle our routes
+    app.get('/',function(req,res){
+        res.render('index.ejs', shopData)
+    });
+    app.get('/about',function(req,res){
+        res.render('about.ejs', shopData);
+    });
+    app.get('/search',function(req,res){
+        res.render("search.ejs", shopData);
+    });
+    app.get('/search-result', function (req, res) {
+        //searching in the database
+        //res.send("You searched for: " + req.query.keyword);
+
+        let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.keyword + "%'"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+            }
+            let newData = Object.assign({}, shopData, {availableBooks:result});
+            console.log(newData)
+            res.render("list.ejs", newData)
+         });        
+    });
+    app.get('/register', function (req,res) {
+        res.render('register.ejs', shopData);                                                                     
+    });                                                                                                 
+    app.post('/registered', function (req,res) {
+        // saving data in database
+
+        let sqlquery = "INSERT INTO users (first, last, userN, password, email) VALUES (?,?,?,?,?)";
+        let newrecord = [req.body.first, req.body.last, req.body.userN, req.body.password, req.body.email];
+
+
+        const bcrypt = require('bcrypt');
+        const saltRounds = 10;
+        const plainPassword = req.body.password;
+     
+
+
+        bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+            // Store hashed password in your database.
+            result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email;
+            result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
+
+            db.query(sqlquery, newrecord, (err, result) => {
+                if (err) {
+                  return console.error(err.message);
+                }
+                else
+                res.send(' This user is added to database, name: '+ req.body.first + ' price '+ req.body.last);
+                
+                });
+
+            
+          })
+          
+
+
+        
+
+                                                                           
+    }); 
+    app.get('/list', function(req, res) {
+        let sqlquery = "SELECT * FROM books"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+            }
+            let newData = Object.assign({}, shopData, {availableBooks:result});
+            console.log(newData)
+            res.render("list.ejs", newData)
+         });
+    });
+
+    app.get('/listusers', function(req, res) {
+        let sqlquery = "SELECT * FROM users"; // query database to get all the users
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+            }
+            let newData = Object.assign({}, shopData, {userlist:result});
+            console.log(newData)
+            res.render("listusers.ejs", newData)
+         });
+    });
+
+    app.get('/addbook', function (req, res) {
+        res.render('addbook.ejs', shopData);
+     });
+ 
+     app.post('/bookadded', function (req,res) {
+           // saving data in database
+           let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+           // execute sql query
+           let newrecord = [req.body.name, req.body.price];
+           db.query(sqlquery, newrecord, (err, result) => {
+             if (err) {
+               return console.error(err.message);
+             }
+             else
+             res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price);
+             });
+       });    
+
+       app.get('/bargainbooks', function(req, res) {
+        let sqlquery = "SELECT * FROM books WHERE price < 20";
+        db.query(sqlquery, (err, result) => {
+          if (err) {
+             res.redirect('./');
+          }
+          let newData = Object.assign({}, shopData, {availableBooks:result});
+          console.log(newData)
+          res.render("bargains.ejs", newData)
+        });
+    });       
+
+}
