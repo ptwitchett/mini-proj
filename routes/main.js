4bd7673bb872fe31b4da5ccf522a1bbd81ff117f
ptwitchett <p.twitchett@aol.co.uk>
2023-12-12 12:25:30 +0000

    main

--- routes/main.js
+++ routes/main.js
@@ -29,17 +29,10 @@ module.exports = function(app, shopData) {
         res.render('register.ejs', shopData);                                                                     
     });                                                                                                 
     app.post('/registered', function (req,res) {
-        // saving data in database
-
-     //   let sqlquery = "INSERT INTO users (first, last, userN, password, email) VALUES (?,?,?,?,?)";
-     //   let newrecord = [req.body.first, req.body.last, req.body.userN, req.body.password, req.body.email];
-
-
         const bcrypt = require('bcrypt');
         const saltRounds = 10;
         const plainPassword = req.body.password;
-     
-
+    
 
         bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
             // Store hashed password in your database.
@@ -62,32 +55,58 @@ module.exports = function(app, shopData) {
 
     app.get('/login', function (req,res) {
         res.render('login.ejs', shopData);                                                                     
-    });                                                                                                 
-    app.post('/loggedin', function (req,res) {
-       
+    });                     
 
-        //const bcrypt = require('bcrypt');
-  
-        //sqlquery = "SELECT password FROM users WHERE userN="+ req.body.userN;
-        //const hashedPassword = 
-
-            // Compare the password supplied with the password in the database
-        bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
+    app.post('/loggedin', function (req, res) {
+        const bcrypt = require('bcrypt');
+        let username = req.body.userN;
+        let enteredPassword = req.body.password;
+    
+        let userQuery = "SELECT * FROM users WHERE userN = ?";
+    
+        // Check if the user exists in the users table
+        db.query(userQuery, [username], (err, results) => {
             if (err) {
                 return console.error(err.message);
-   
             }
-            else if (result == true) {
-            res.send('Welcome' + req.body.first + ' ' + req.body.last);
+    
+            if (results.length === 0) {
+                return res.send('User does not exist');
             }
-            else {
-                res.send('Incorrect password username combination');
+    
+          
+            let hashedPassword = results[0].password;
+    
+            // Compare the password supplied with the password in the database
+            bcrypt.compare(enteredPassword, hashedPassword, function(compareErr, result) {
+                if (compareErr) {
+                    
+                    return console.error(compareErr.message);
+                }
+                else if (result == true) {
+                    
+                    res.send('Welcome ' + results[0].first + ' ' + results[0].last);
+                }
+                else {
+                    
+                    res.send('Incorrect password username combination');
+                }
+            });
+        });
+    });
+    app.get('/bookings', function(req, res) {
+        let sqlquery = "SELECT * FROM bookings"; // query database to get all the hotels
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
             }
-      });
+            let newData = Object.assign({}, shopData, {availablebookings:result});
+            console.log(newData) 
+            res.render("bookings.ejs", newData)
+         });
+    });
   
-                                                                             
-    }); 
-
     app.get('/list', function(req, res) {
         let sqlquery = "SELECT * FROM hotels"; // query database to get all the hotels
         // execute sql query
@@ -100,29 +119,27 @@ module.exports = function(app, shopData) {
             res.render("list.ejs", newData)
          });
     });
-
-
     app.post('/booked', function (req, res) {
         let userCheckQuery = "SELECT * FROM users WHERE userN = ?";
         let hotelCheckQuery = "SELECT * FROM hotels WHERE name = ?";
         let insertBookingQuery = "INSERT INTO bookings (userN, name) VALUES (?, ?)";
+        let updateHotelQuery = "UPDATE hotels SET booked = 'Reserved' WHERE name = ?";
     
         // Check if userN exists in the users table
         db.query(userCheckQuery, req.body.userN, (userErr, userResult) => {
             if (userErr) {
-                return console.error(err.message);
+                return console.error(userErr.message);
             }
             if (userResult.length === 0) {
-                return console.error(err.message);
+                return console.error('User does not exist');
             }
-    
             // Check if hotels exist in the hotels table
             db.query(hotelCheckQuery, [req.body.hotels], (hotelErr, hotelResult) => {
                 if (hotelErr) {
-                    return console.error(err.message);
+                    return console.error(hotelErr.message);
                 }
                 if (hotelResult.length === 0) {
-                    return console.error(err.message);
+                    return console.error('Hotel does not exist');
                 }
     
                 // Insert booking information into the bookings table
@@ -130,15 +147,18 @@ module.exports = function(app, shopData) {
                     if (insertErr) {
                         return console.error(insertErr.message);
                     }
-                    res.send('Thank you ' + req.body.userN + ' for booking a hotel room at ' + req.body.hotels);
+    
+                    // Update the 'booked' status in the hotels table to 'Reserved'
+                    db.query(updateHotelQuery, [req.body.hotels], (updateErr, updateResult) => {
+                        if (updateErr) {
+                            return console.error(updateErr.message);
+                        }
+                        res.send('Thank you ' + req.body.userN + ' for booking a hotel room at ' + req.body.hotels);
+                    });
                 });
             });
         });
     });
-    
-
-
-
 
     app.get('/listusers', function(req, res) {
         let sqlquery = "SELECT * FROM users"; // query database to get all the users
@@ -153,3 +173,25 @@ module.exports = function(app, shopData) {
          });
     });
 
+    app.get('/weather',function(req,res){
+        const request = require('request');
+          
+        let apiKey = '6c42897ec7f6f25d10afd951e612ae1e';
+        let city = 'london';
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+                 
+        request(url, function (err, response, body) {
+        if(err){
+            console.log('error:', error);
+        } else {
+            var weather = JSON.parse(body)
+            var wmsg = 'It is '+ weather.main.temp + 
+            ' degrees in '+ weather.name +
+            '! <br> The humidity now is: ' + 
+            weather.main.humidity;
+            res.send (wmsg);
+
+        } 
+        });
+    });   
+}
